a50b66176fef2274b852fc77da3ee8e2d8cef285
sarastk <124815899+sarastk@users.noreply.github.com>
2023-05-03 21:01:44 +0300

    Update winner.py

--- modules/winner.py
+++ modules/winner.py
@@ -1,10 +1,13 @@
+# len(grid)    : αριθμός γραμμών
+# len(grid[0]) : αριθμός στηλών
+
 # Ελέγχει αν υπάρχουν 4 όμοιες θέσεις οριζόντια
 def horizontal_four(grid):
-    # για κάθε γραμμή (0 έως 5)
-    # ελέγχει τις θέσεις των στηλών (0 έως 3),(1 έως 4),(2 έως 5),(3 έως 6)
-    for row in range(0, 6):
+    # για κάθε γραμμή (0 έως και την τελευταία)
+    # ελέγχει τις θέσεις των στηλών (0 έως 3),(1 έως 4),(2 έως 5),(3 έως 6),...
+    for row in range(len(grid)):
         #  η αρχική στήλη της τετράδας column
-        for column in range(0, 4):
+        for column in range(len(grid[0])-3):
             # αν 4 διαδοχικές θέσεις μιας γραμμής είναι ίδιες
             four_same = (grid[row][column] == grid[row][column + 1]) and \
                         (grid[row][column + 1] == grid[row][column + 2]) and \
@@ -19,11 +22,11 @@ def horizontal_four(grid):
 
 # Ελέγχει αν υπάρχουν 4 όμοιες θέσεις κάθετα
 def vertical_four(grid):
-    # για κάθε στήλη (0 έως και 6)
-    # ελέγχει τις θέσεις των γραμμών (0 έως 3),(1 έως 4),(2 έως 5)
-    for column in range(0, 7):
+    # για κάθε στήλη (0 έως και την τελευταία)
+    # ελέγχει τις θέσεις των γραμμών (0 έως 3),(1 έως 4),(2 έως 5),...
+    for column in range(len(grid[0])):
         # η αρχική γραμμή της τετράδας row
-        for row in range(0, 3):
+        for row in range(len(grid)-3):
             # αν 4 διαδοχικές θέσεις μιας στήλης είναι ίδιες
             four_same = (grid[row][column] == grid[row + 1][column]) and \
                         (grid[row + 1][column] == grid[row + 2][column]) and \
@@ -38,10 +41,10 @@ def vertical_four(grid):
 
 # Ελέγχει αν υπάρχουν 4 όμοιες θέσεις στις κυρίως διαγώνιους
 def diagonal_four(grid):
-    # για κάθε γραμμή που μπορεί να ξεκινήσει διαγώνιος (0 έως 2)
+    # για κάθε γραμμή που μπορεί να ξεκινήσει διαγώνιος (0 έως και 3 πριν το τέλος)
     # ελέγχει τις διαδοχικές διαγώνιες θέσεις
-    for row in range(0, 3):
-        for column in range(0, 4):
+    for row in range(len(grid)-3):
+        for column in range(len(grid[0])-3):
             # αν 4 διαδοχικές διαγώνιες θέσεις είναι ίδιες
             four_same = (grid[row][column] == grid[row + 1][column + 1]) and \
                         (grid[row + 1][column + 1] == grid[row + 2][column + 2]) and \
@@ -54,22 +57,12 @@ def diagonal_four(grid):
     return None  # δε βρέθηκε νικητής
 
 
-# Ελέγχει αν υπάρχουν 4 όμοιες θέσεις αντίθετες διαγώνιους
+# Αντιστρέφει το ταμπλό (ως προς τις στήλες)
+# η τελευταία στήλη γίνεται πρώτη, η προτελευταία δεύτερη κτλ.
+# Ελέγχει αν υπάρχουν 4 όμοιες θέσεις στις κυρίως διαγώνιους
 def anti_diagonal_four(grid):
-    # για κάθε γραμμή που μπορεί να ξεκινήσει διαγώνιος (3 έως 5)
-    # ελέγχει τις αντίθετες διαδοχικές διαγώνιες θέσεις
-    for row in range(3, 6):
-        for column in range(0, 4):
-            # αν 4 διαδοχικές αντίθετες διαγώνιες θέσεις είναι ίδιες
-            four_same = (grid[row][column] == grid[row - 1][column + 1]) and \
-                        (grid[row - 1][column + 1] == grid[row - 2][column + 2]) and \
-                        (grid[row - 2][column + 2] == grid[row - 3][column + 3])
-            # αν οι 4 θέσεις δεν είναι κενές
-            if four_same and grid[row][column] != 0:
-                # επιστρέφει το νικητή
-                winner = grid[row][column]
-                return winner
-    return None  # δε βρέθηκε νικητής
+    anti_grid = [[grid[row][column] for column in range(len(grid[0])-1, -1, -1)] for row in range(len(grid))]
+    return diagonal_four(anti_grid)
 
 
 # Ελέγχει αν υπάρχει νικητής και τον επιστρέφει
@@ -108,25 +101,37 @@ def is_draw(grid):
 
 # Παίρνει ως όρισμα το ταμπλό και τους παίκτες
 # Ελέγχει αν υπάρχει νικητής/ισοπαλία και επιστρέφει True/False
-def game_over(grid, player1, player2):
+def game_over(grid):
     winner = check_winner(grid)
-    if winner == 1:
-        # κέρδισε ο παίκτης 1
-        print(f"---{player1.name} Wins---")
-        player1.win()
-        player2.lose()
-        return True
-    elif winner == 2:
-        # κέρδισε ο παίκτης 2
-        print(f"---{player2.name} Wins---")
-        player2.win()
-        player1.lose()
-        return True
-    elif is_draw(grid):
-        # ισοπαλία
-        print(f"------ Draw ------")
-        player1.draw()
-        player2.draw()
-        return True
+
+    if winner == 1:  # κέρδισε ο παίκτης 1
+        return winner
+    elif winner == 2:  # κέρδισε ο παίκτης 2
+        return winner
+    elif is_draw(grid):  # ισοπαλία
+        return 0
     # δεν υπάρχει νικητής/ισοπαλία
     return False
+
+
+# Ενημέρωση στατιστικών παικτών
+def update_stats(winner, player1, player2):
+    if winner == 1:
+        player1.win()
+        player2.lose()
+    elif winner == 2:
+        player2.win()
+        player1.lose()
+    elif winner == 0:
+        player2.draw()
+        player1.draw()
+
+
+# Εμφάνιση νικητή
+def print_winner(winner, player1, player2):
+    if winner == 1:
+        print(f"---{player1.name} Wins---")
+    elif winner == 2:
+        print(f"---{player2.name} Wins---")
+    elif winner == 0:
+        print(f"------ Draw ------")
