1aeaed41c7054d8c96f46a7bd042dfeeee16478c
sarastk <124815899+sarastk@users.noreply.github.com>
2023-05-10 21:04:21 +0300

    Update player.py

--- modules/player.py
+++ modules/player.py
@@ -1,14 +1,11 @@
 class Player:
-    player_list = []    # λίστα με όλους τους παίκτες
-
     def __init__(self, name):
         self.name = name    # όνομα
         self.games = 0      # αριθμός παιχνιδιών
         self.wins = 0       # νίκες
-        self.loss = 0       # ήττες
+        self.losses = 0     # ήττες
         self.draws = 0      # ισοπαλίες
-        self.score = 0      # δείκτης αξιολόγησης επίδοσης(νίκη+1/ήττα-1/ισοπαλία 0)
-        Player.player_list.append(self)    # προσθήκη παίκτη στη λίστα
+        self.elo = 0        # δείκτης αξιολόγησης επίδοσης(νίκη+1/ήττα-1/ισοπαλία 0)
 
     # Ο παίκτης κερδίζει
     def win(self):
@@ -17,16 +14,16 @@ class Player:
         # αύξηση αριθμού παιχνιδιών
         self.games += 1
         # ο δείκτης αξιολόγησης αυξάνεται κατά 1
-        self.score += 1
+        self.elo += 1
 
     # Ο παίκτης χάνει
     def lose(self):
         # οι ήττες αυξάνονται κατά 1
-        self.loss += 1
+        self.losses += 1
         # αύξηση αριθμού παιχνιδιών
         self.games += 1
         # ο δείκτης αξιολόγησης μειώνεται κατά 1
-        self.score -= 1
+        self.elo -= 1
 
     # Ισοπαλία
     def draw(self):
@@ -38,9 +35,12 @@ class Player:
 
     # παρουσίαση παίκτη
     def __str__(self):
-        return f"{self.name:<20}{self.score:<7}{self.games:<7}{self.wins:<7}{self.loss:<7}{self.draws:<7}"
+        return f"{self.name:<20}{self.elo:<7}{self.games:<7}{self.wins:<7}{self.losses:<7}{self.draws:<7}"
 
     # Επιστρέφει tuple με τα στοιχεία του παίκτη
     def get_att(self):
-        return self.name, self.games, self.wins, self.loss, self.draws, self.score
+        return self.games, self.wins, self.losses, self.draws, self.elo, self.name
+
+
+
 
