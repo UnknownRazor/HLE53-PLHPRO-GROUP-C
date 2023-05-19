341427c959c4e6e26ee5b58094d498e5dc1b86a2
sarastk <124815899+sarastk@users.noreply.github.com>
2023-05-19 19:00:55 +0300

    Update game.py

--- modules/game.py
+++ modules/game.py
@@ -178,7 +178,7 @@ class Game:
             if computer_row != -1:
                 break
         # ο υπολογιστής καταλαμβάνει την κενή θέση
-        self.grid[computer_row][computer_column] = player
+        self.grid[computer_row][computer_column] = COMPUTER
         return computer_column
 
 
