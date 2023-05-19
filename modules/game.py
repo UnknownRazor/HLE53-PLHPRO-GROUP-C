20494ebe41f164cc3c83f3da450759b4968c336b
sarastk <124815899+sarastk@users.noreply.github.com>
2023-05-19 19:00:06 +0300

    Update game.py

--- modules/game.py
+++ modules/game.py
@@ -207,7 +207,7 @@ class Game:
             return -1
         else:
             # ο παίκτης καταλαμβάνει την κενή θέση
-            self.grid[player_row][player_column] = player1
+            self.grid[player_row][player_column] = PLAYER1
             return player_row
 
     # Ελέγχει αν υπάρχουν 4 όμοιες θέσεις οριζόντια
